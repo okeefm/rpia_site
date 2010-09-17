fd458ca6f5107f1bfc5af42b3fc5da507e69ab19
Michael O'Keefe <okeefm@rpi.edu>
2010-09-17 00:42:43 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RpiaSite::Application.config.secret_token = '0ea9b5c0ff2b59da6a29c9bb9115936a546a25a3e05196e0afa7220fc56ef0da691a6fcbdb9f113b0de364aa7e6452ef832b2a86970dc7818656213b0657816b'
